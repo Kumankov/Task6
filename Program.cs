5a21260084edfb87b1087eddd558c11cb4714dd7
Ilya <kumankov@gmail.ru>
2022-11-18 22:58:22 +0300

    11

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿ Console.WriteLine("введите число:");
+int N = int.Parse(Console.ReadLine());
+if ((N % 2)==0)
+ Console.WriteLine("Введённое число четное");
+ else
+  Console.WriteLine("Введённое число нечетное");
+ 
